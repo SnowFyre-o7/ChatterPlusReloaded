87a12a1d2db6aba44634ce52a3743dc006e3ea8c
SnowFyre-o7 <118649127+SnowFyre-o7@users.noreply.github.com>
2022-11-20 16:12:30 +0530

    Add files via upload

---
+++ src/main/java/net/snowfyre/chatterplusreload/Utils.java
@@ -0,0 +1,14 @@
+package net.snowfyre.chatterplusreload;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.metadata.MetadataValue;
+
+public class Utils {
+
+    public static String color(String s) {
+
+        return ChatColor.translateAlternateColorCodes('&', s);
+    }
+    }
+
